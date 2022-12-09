afe387f70f008b802784ea0535bb82d379387dc0
nitzanmr <81086233+nitzanmr@users.noreply.github.com>
2022-12-09 09:22:20 +0000

    changed the comment uptop.

--- Client.c
+++ Client.c
@@ -1,7 +1,8 @@
-* // write parsing function that takes argv given in the main to it and parase
-  // it and make the http command.*//
+/* write parsing function that takes argv given in the main to it and parase
+   it and make the http command.*/
 #include "string.h"
-char* http_parsing(char *argv[]) {
+
+char* http_parsing(char *argv[]){
   int flag_r = 0;
   int flag_p = 0;
   int flag_in_url = 0;
@@ -18,14 +19,16 @@ char* http_parsing(char *argv[]) {
     if (flag_r == 1) {
       number_of_parameters = atoi(argv[i]);///enter the number of parameters into placeholder
       flag_r = 0;
-    } else if (number_of_parameters >= 1) {
+    } 
+    else if (number_of_parameters >= 1) {
       strcat(parmeters_of_r, strcat(argv[i] + '&'));
       ///check for the parameters of -r
       number_of_parameters--;
       if (number_of_parameters == 0)///for making it not possible to enter -r twice.
         number_of_parameters = -1;
-    } else {
-      if(strstr(argv[i],"http://"){
+    } 
+    else {
+      if(strstr(argv[i],"http://")){
         flag_in_url = 1;
         for (int j = 0; argv[i][j] != NULL; j++) {
           if (flag_in_url == 1) {
@@ -36,7 +39,8 @@ char* http_parsing(char *argv[]) {
               // checks if we are at the end of the url.
               flag_in_url = 0;
             }
-          } else {
+          } 
+          else {
             if (argv[i][j] == ':') { // check if there is a spacifec port.
               whlie(argv[i][j] != '/') {
                 strcat(port, argv[i][j]);
