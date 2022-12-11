688b2cf8a3c9e73a2413ba41a8b190d50e1107b6
nitzanmr <81086233+nitzanmr@users.noreply.github.com>
2022-12-11 17:41:39 +0000

    added the print of the url in the main

--- main.c
+++ main.c
@@ -4,5 +4,5 @@ int main(void) {
   char* argv[5] = {"-r","2","city=dallas","state=texas","http://www.ptsv2.com:8/t/ex2"};
   Client* new_client;
   new_client = http_parsing(5,argv);
-  printf("%s",new_client->parameters_of_r);
+  printf("the parameters are: %s\nthe host is: %s",new_client->parameters_of_r,new_client->url);
 }
