40df9998c116580889a13ab936db3686e37886b4
nitzanmr <81086233+nitzanmr@users.noreply.github.com>
2022-12-11 14:56:54 +0000

    fixing the parsing function and checking the values getting from http alone. next check for -r -p

--- Client.c
+++ Client.c
@@ -13,15 +13,15 @@ struct Client
 }typedef Client;
 
 void client_init(Client* new_client){
-  new_client->parameters_of_r =(char*)malloc(sizeof(char*));
-  new_client->path = (char*)malloc(sizeof(char*));
-  new_client->path[0] = '/';
-  new_client->port = 0;
-  new_client->text = (char*)malloc(sizeof(char*));
-  new_client->url = (char*)malloc(sizeof(char*));
+  new_client->parameters_of_r =(char*)malloc(sizeof(char));
+  new_client->path = (char*)malloc(sizeof(char));
+  new_client->path[0] = '\0';
+  new_client->port = 80;
+  new_client->text = (char*)malloc(sizeof(char));
+  new_client->url = (char*)malloc(sizeof(char));
   new_client->text_length = 0;
 }
-char** http_parsing(char *argv[]){
+Client* http_parsing(int length_of_argv,char *argv[]){
   int flag_r = 0;
   int flag_p = 0;
   int flag_in_url = 0;
@@ -32,7 +32,7 @@ char** http_parsing(char *argv[]){
   Client* new_client = (Client*)malloc(sizeof(Client));
   client_init(new_client);
   char *check_com = (char*)malloc(sizeof(char*));/*check for if the value inside the http:// if we arrived to the value .com*/
-  for (int i = 0; argv[i] != NULL; i++) {
+  for (int i = 0; i<length_of_argv; i++) {
     if (flag_p == 1) {/*checks for the value of the flag of p and adds the text that comes afterwards to the */
       size_of_text = atoi(argv[i]);/*checks the value of the size of the text.*/
       i++;
@@ -49,8 +49,8 @@ char** http_parsing(char *argv[]){
       flag_r = 0;
     } 
     else if (number_of_parameters >= 1) {
-      char* and_char = "&";
-      strcat(argv[i] ,and_char);
+      char and_char = '&';
+      strncat(argv[i] ,and_char,1);
       strcat(new_client->parameters_of_r, argv[i]);
       ///check for the parameters of -r
       number_of_parameters--;
@@ -58,7 +58,7 @@ char** http_parsing(char *argv[]){
         number_of_parameters = -1;
     } 
     else {
-      if(strstr(argv[i],"http://")){
+      if(strstr(argv[i],"http://")!=NULL){
         flag_in_url = 1;
         for (int j = 0; argv[i][j] != NULL; j++) {
           if (flag_in_url == 1) {
@@ -66,30 +66,34 @@ char** http_parsing(char *argv[]){
             strncat(new_client->url, &argv[i][j],1);
             if(strlen(check_com)>=4){
               /*checks if the number of values in check_com is more than 4 to delete the first one and add the new one to the end*/
-              for (int i = 0; i < 4; i++)
+              for (int k = 0; k < 4; k++)
               {
-                check_com[i] = check_com[i+1];
+                check_com[k] = check_com[k+1];
               }
             }
             strncat(check_com,&argv[i][j],1);
-            if (strcmp(check_com, ".com")) {
+            if (strcmp(check_com, ".com")==0) {
               // checks if we are at the end of the url.
               flag_in_url = 0;
             }
           } 
           else {
-            if (argv[i][j] == ':') { // check if there is a spacific port.
+            if (argv[i][j] == ':'&& j>5) { // check if there is a spacific port.
               char* temp_for_port = "";
               while(argv[i][j] != '/'&&argv[i][j]!=NULL){
                 strncat(temp_for_port,&argv[i][j],1);
                 j++;
               }
-              port = atoi(temp_for_port);
+              new_client->port = atoi(temp_for_port);
             }
             else{/*adds the value of the path to the placeholder char* path*/
               strncat(new_client->path,&argv[i][j],1);
             }
           }
+        }
+        if(new_client->path[0]=='\0'){
+          char temp = '/';
+          strncat(new_client->path,&temp,1);
         }
       }
       else{
@@ -109,5 +113,5 @@ char** http_parsing(char *argv[]){
       }
     }
   }
-
+  return new_client;
 }
--- main.c
+++ main.c
@@ -1,18 +1,7 @@
 #include "Client.c"
 #include "malloc.h"
 int main(void) {
-  char* new = (char*)malloc(sizeof(char*));
-  new[0] = '/';
-  char new_a = 'a';
-  char* new_abv= "abv";
-  char new_cc[3][2];
-  strcpy(new_cc[0],"11");
-  // new_cc[0] = "11";
-  // new_cc[1] = "11";
-  strncat(new,&new_cc[0][0],1);
-  // strncat(new,&new_abv,strlen(new_abv)-1);
-  // for(int i =0;i<strlen(new_abv)-1;i++){
-  //   strncat(new,&new_abv[i],1);
-  // }
-  printf("%s",new);
+  char* argv[1] = {"http://www.ptsv2.com/t/ex2"};
+  Client* new_client;
+  http_parsing(1,argv);
 }
